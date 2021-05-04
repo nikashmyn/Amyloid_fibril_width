95107ed3572ad7d3c4f6ab139dc5a96c6fa0d572
nikashmyn <67345995+nikashmyn@users.noreply.github.com>
2021-05-04 15:03:47 -0400

    Added the plotting to the macro bash script.

--- Generate_H-Bond_Plots.py
+++ Generate_H-Bond_Plots.py
@@ -13,7 +13,7 @@
 
 # ### Imports
 
-# In[24]:
+# In[1]:
 
 
 import pandas as pd
@@ -32,24 +32,27 @@ pd.set_option('display.max_rows', 200)
 
 # ### Read in cmd line args
 
-# In[25]:
+# In[2]:
 
+
+#sh macro_hbond.sh /Users/NikosMynhier/Desktop/David/Alpha_Psi_Calculation/fixed_pdbs_h 8 /Users/NikosMynhier/Desktop/code/2021/AB-Fibril-Radius-vs-Hydrogen-Bond-Relationship /Users/NikosMynhier/Desktop/David/plots /Users/NikosMynhier/Desktop/David/interstrand_radius /Users/NikosMynhier/Desktop/David/helical_parameters.txt 
 
 args = sys.argv
 #args: script name, /path/to/file.pdb, /path/to/uncert_file.pdb
-args = ["script name", "/Users/NikosMynhier/Desktop/David/Alpha_Psi_Calculation/fixed_pdbs_h/6ufr_origin_5layers_h.datatable.csv", "/Users/NikosMynhier/Desktop/David/Alpha_Psi_Calculation/fixed_pdbs_h/6ufr_origin_5layers_h.hbonds.csv", "/Users/NikosMynhier/Desktop/David/interstrand_radius/6ufr_origin_5layers_rad_dist.csv", "/Users/NikosMynhier/Desktop/David/helical_parameters.txt"]
+#args = ["script name", "/Users/NikosMynhier/Desktop/David/Alpha_Psi_Calculation/fixed_pdbs_h/6ufr_origin_5layers_h.datatable.csv", "/Users/NikosMynhier/Desktop/David/Alpha_Psi_Calculation/fixed_pdbs_h/6ufr_origin_5layers_h.hbonds.csv", "/Users/NikosMynhier/Desktop/David/interstrand_radius/6ufr_origin_5layers_rad_dist.csv", "/Users/NikosMynhier/Desktop/David/helical_parameters.txt", "/Users/NikosMynhier/Desktop/David/plots"]
 #"5w7v_3layers_neg120p44_h.pdb" #'6ufr_origin_5layers_h.pdb'
 
 
-# In[26]:
+# In[3]:
 
 
 sample_name1 = args[1].split("/")[-1][0:4]
 sample_name2 = args[2].split("/")[-1][0:4]
 path = args[1][0:len(args[1])-len(args[1].split("/")[-1])]
+outdir = args[5]
 
 
-# In[27]:
+# In[4]:
 
 
 filename = args[1]
@@ -61,7 +64,7 @@ h_bonds = pd.read_csv(filename2)
 
 # ### Plotting
 
-# In[28]:
+# In[5]:
 
 
 colors = []
@@ -87,7 +90,7 @@ for i in range(len(h_bonds.iloc[:,1])):
 # 
 # ---
 
-# In[29]:
+# In[6]:
 
 
 filenames = args[3]
@@ -98,7 +101,7 @@ dataframe_tilt = h_bonds["tilt"]
 helix = pd.read_csv(filenames_helical, delimiter='\t', skipinitialspace=True, header=None)
 
 
-# In[30]:
+# In[7]:
 
 
 def get_radius(dataframe):
@@ -135,7 +138,7 @@ for j in range(len(dataframe)):
 #    Theo_tilt.append( pred_tilt(x, y, dataframe_tilt[j]) )
 
 
-# In[32]:
+# In[8]:
 
 
 figsize = (15,5)
@@ -146,15 +149,17 @@ fig, ax = plt.subplots(rows, columns, figsize=figsize, constrained_layout=True)
 fig.suptitle("All Relationships & Fits", fontsize=20)
 
 #Plot all Radius vs Distance
-ax[0].scatter(h_bonds['radius'], h_bonds['dist'], c=colors)
-ax[0].legend(['Red = Beta-Beta, Blue = else'], frameon=False, loc='upper right')
+#ax[0].scatter(h_bonds['radius'], h_bonds['dist'], c=colors)
+ax[0].scatter(radius_onlyBB, dist_onlyBB, c=colors_onlyBB)
+ax[0].legend(['Red = Beta-Beta'], frameon=False, loc='upper right') #, Blue = else'
 ax[0].set_title(sample_name1, fontsize=15)
 ax[0].set_xlabel("Radius", fontsize=15)
 ax[0].set_ylabel("Dist", fontsize=15)
 
 #Plot all Radius vs Tilt
-ax[1].scatter(h_bonds['radius'], h_bonds['tilt'], c=colors)
-ax[1].legend(['Red = Beta-Beta, Blue = else'], frameon=False, loc='upper right')
+#ax[1].scatter(h_bonds['radius'], h_bonds['tilt'], c=colors)
+ax[1].scatter(radius_onlyBB, tilt_onlyBB, c=colors_onlyBB)
+ax[1].legend(['Red = Beta-Beta'], frameon=False, loc='upper right') #, Blue = else'
 ax[1].set_title(sample_name1, fontsize=15)
 ax[1].set_xlabel("Radius", fontsize=15)
 ax[1].set_ylabel("Tilt", fontsize=15)
@@ -168,8 +173,8 @@ ax[2].set_title(name, fontsize=15)
 ax[2].set_xlabel("Radius (Å)", fontsize=15)
 ax[2].set_ylabel("i, i+1 distance (Å)", fontsize=15)
 
-plt.savefig(f"{path}/{sample_name1}.all_plots.pdf")
-plt.show() 
+plt.savefig(f"{outdir}/{sample_name1}.all_plots.pdf")
+#plt.show() 
 
 
 # In[ ]:
@@ -194,44 +199,44 @@ plt.show()
 # In[55]:
 
 
-figsize = (15,5)
-columns = 2
-rows = 1
-
-fig, ax = plt.subplots(rows, columns, figsize=figsize, constrained_layout=True)
-fig.suptitle("All Relationships & Fits")
-
-#Plot Beta-Beta radius vs. dsitance w/ quadratic fit 
-[a, b, c] = np.polyfit(radius_onlyBB, dist_onlyBB, 2)
-x2d = np.linspace(0,50,100)
-y2d = a*(x2d**2) + b*x2d + c
-
-[a, b] = np.polyfit(radius_onlyBB, dist_onlyBB, 1)
-x1d = np.linspace(0,50,100)
-y1d = a*x1d + b
-
-ax[0].scatter(radius_onlyBB, dist_onlyBB, c=colors_onlyBB)
-ax[0].plot(x2d, y2d)
-ax[0].set_title(f'{filename} only Beta-Beta', fontsize=15)
-ax[0].set_xlabel("Radius", fontsize=15)
-ax[0].set_ylabel("Dist", fontsize=15)
-
-#Plot Beta-Beta Radius vs Tilt w/ linear fit
-[a, b, c] = np.polyfit(radius_onlyBB, tilt_onlyBB, 2)
-x2d = np.linspace(0,50,100)
-y2d = a*(x2d**2) + b*x2d + c
-
-[a, b] = np.polyfit(radius_onlyBB, tilt_onlyBB, 1)
-x1d = np.linspace(0,50,100)
-y1d = a*x1d + b
-
-ax[1].scatter(radius_onlyBB, tilt_onlyBB, c=colors_onlyBB)
-ax[1].plot(x1d, y1d)
-ax[1].set_title(f'{filename} only Beta-Beta', fontsize=15)
-ax[1].set_xlabel("Radius", fontsize=15)
-ax[1].set_ylabel("Tilt", fontsize=15)
-
-plt.show() 
+#figsize = (15,5)
+#columns = 2
+#rows = 1
+#
+#fig, ax = plt.subplots(rows, columns, figsize=figsize, constrained_layout=True)
+#fig.suptitle("All Relationships & Fits")
+#
+##Plot Beta-Beta radius vs. dsitance w/ quadratic fit 
+#[a, b, c] = np.polyfit(radius_onlyBB, dist_onlyBB, 2)
+#x2d = np.linspace(0,50,100)
+#y2d = a*(x2d**2) + b*x2d + c
+#
+#[a, b] = np.polyfit(radius_onlyBB, dist_onlyBB, 1)
+#x1d = np.linspace(0,50,100)
+#y1d = a*x1d + b
+#
+#ax[0].scatter(radius_onlyBB, dist_onlyBB, c=colors_onlyBB)
+#ax[0].plot(x2d, y2d)
+#ax[0].set_title(f'{filename} only Beta-Beta', fontsize=15)
+#ax[0].set_xlabel("Radius", fontsize=15)
+#ax[0].set_ylabel("Dist", fontsize=15)
+#
+##Plot Beta-Beta Radius vs Tilt w/ linear fit
+#[a, b, c] = np.polyfit(radius_onlyBB, tilt_onlyBB, 2)
+#x2d = np.linspace(0,50,100)
+#y2d = a*(x2d**2) + b*x2d + c
+#
+#[a, b] = np.polyfit(radius_onlyBB, tilt_onlyBB, 1)
+#x1d = np.linspace(0,50,100)
+#y1d = a*x1d + b
+#
+#ax[1].scatter(radius_onlyBB, tilt_onlyBB, c=colors_onlyBB)
+#ax[1].plot(x1d, y1d)
+#ax[1].set_title(f'{filename} only Beta-Beta', fontsize=15)
+#ax[1].set_xlabel("Radius", fontsize=15)
+#ax[1].set_ylabel("Tilt", fontsize=15)
+#
+#plt.show() 
 
 
 # ---
@@ -242,49 +247,49 @@ plt.show()
 # In[56]:
 
 
-# Weighting linearly or with uncertainty
-
-#Linear Weighting
-def linear_weight(radius):
-    return (1-(1/radius))
-
-dist_err = []
-for rad in radius_onlyBB:
-    dist_err.append(.0005*linear_weight(rad))
-#derr = np.array(radius_onlyBB)*np.array(dist_err)
-
-#Uncertainty
-derr = uncert_onlyBB
+## Weighting linearly or with uncertainty
+#
+##Linear Weighting
+#def linear_weight(radius):
+#    return (1-(1/radius))
+#
+#dist_err = []
+#for rad in radius_onlyBB:
+#    dist_err.append(.0005*linear_weight(rad))
+##derr = np.array(radius_onlyBB)*np.array(dist_err)
+#
+##Uncertainty
+#derr = uncert_onlyBB
 
 
 # In[57]:
 
 
-def curve(x, a, b, c):
-    return a*(x**2) + b*x + c
-
-x = radius_onlyBB
-y = dist_onlyBB
-plt.errorbar(x, y, yerr=derr, fmt=".", label="Data")
-popt, pcov = sp.curve_fit(curve, x, y, sigma=derr)
-
-print("a =", popt[0], "+/-", pcov[0,0]**0.5)
-print("b =", popt[1], "+/-", pcov[1,1]**0.5)
-print("c =", popt[2], "+/-", pcov[2,2]**0.5)
-print('Weighted fit parameters:', popt)
-print('Covariance matrix:'); print(pcov)
-
-rangerad = np.linspace(0, 50)
-plt.plot(curve(rangerad, popt[0], popt[1], popt[2]), label="Fit")
-
-
-#Graphics
-plt.title("Radius vs. H-bond Distance")
-plt.xlabel("Radius (angstroms)")
-plt.ylabel("H-bond Distance (angstroms)")
-plt.legend(loc='upper right')
-#plt.savefig('/Users/NikosMynhier/Desktop/X_vs_lnI_Graph')
-plt.show()
+#def curve(x, a, b, c):
+#    return a*(x**2) + b*x + c
+#
+#x = radius_onlyBB
+#y = dist_onlyBB
+#plt.errorbar(x, y, yerr=derr, fmt=".", label="Data")
+#popt, pcov = sp.curve_fit(curve, x, y, sigma=derr)
+#
+#print("a =", popt[0], "+/-", pcov[0,0]**0.5)
+#print("b =", popt[1], "+/-", pcov[1,1]**0.5)
+#print("c =", popt[2], "+/-", pcov[2,2]**0.5)
+#print('Weighted fit parameters:', popt)
+#print('Covariance matrix:'); print(pcov)
+#
+#rangerad = np.linspace(0, 50)
+#plt.plot(curve(rangerad, popt[0], popt[1], popt[2]), label="Fit")
+#
+#
+##Graphics
+#plt.title("Radius vs. H-bond Distance")
+#plt.xlabel("Radius (angstroms)")
+#plt.ylabel("H-bond Distance (angstroms)")
+#plt.legend(loc='upper right')
+##plt.savefig('/Users/NikosMynhier/Desktop/X_vs_lnI_Graph')
+#plt.show()
 
 
 # In[ ]:
--- macro_hbond.sh
+++ macro_hbond.sh
@@ -4,6 +4,8 @@ datadir=$1
 cpus=$2
 scriptsdir=$3 #path to the folder which includes all auxiliary scripts
 outdir=$4
+distdir=$5
+helixdir=$6
 
 echo ${datadir}
 echo ${cpus}
@@ -11,11 +13,12 @@ echo ${cpus}
 echo "Calculating h-bonds and generating visuals for the following files:"
 echo $PWD
 
-#clear old results from outdir
-rm ${outdir}/*
-
 cd ${datadir}
-#ls ${datadir}/*.pdb
+ls ${datadir}/*.pdb
+
+#clear old results from outdir
+#i dont think we need to remove old results. It should save over. uncomment if im wrong. 
+#rm ${outdir}/*
 
 #read in .pdb files to process
 ls ${datadir}/*.pdb > ${outdir}/pdb_files.txt
@@ -23,7 +26,7 @@ ls ${datadir}/*.pdb > ${outdir}/pdb_files.txt
 #Reset the cmd list file
 echo "" > ${outdir}/cmd_list.cmds
 
-#Run script to generate h-bonds csvs
+#Generate Run script commands (cmds) for h-bonds csvs
 while read line  
 do
   echo "python3 ${scriptsdir}/find_hbonds_04252021_debugged.py ${line} ${outdir}" >> ${outdir}/cmd_list.cmds;
@@ -32,20 +35,21 @@ done < ${outdir}/pdb_files.txt
 
 parallel --jobs ${cpus} < ${outdir}/cmd_list.cmds &> ${outdir}/run.macro_hbond.stdouterr
 
+#Reset the cmd list file
+echo "" > ${outdir}/cmd_list_plots.cmds
 
 #read in successfully generated csvs 
-#ls ${outdir}/*datatable.csv > ${outdir}/datatables_files.txt
-#ls ${outdir}/*hbonds.csv > ${outdir}/hbonds_files.txt
+ls ${outdir}/*datatable.csv > ${outdir}/datatables_files.txt
+ls ${outdir}/*hbonds.csv > ${outdir}/hbonds_files.txt
+ls ${distdir}/*dist.csv > ${outdir}/dist_files.txt
 
-#Run script to generate h-bonds plots
-#while read line
-#do
-#  echo "python3 ${scriptsdir}/Generate_H-Bond_Plots.ipynb ${line} ${outdir}" >> ${outdir}/cmd_list.cmds;
-#  echo "" >> ${outdir}/cmd_list.cmds;
-#done < ${outdir}/pdb_files.txt
+#Generate run script commands (cmds) for h-bond plots
+while read line1 <&1 && read line2 <&2 && read line3 <&3; 
+do
+  echo "python3 ${scriptsdir}/Generate_H-Bond_Plots.py ${line1} ${line2} ${line3} ${helixdir} ${outdir}" >> ${outdir}/cmd_list_plots.cmds;
+  echo "" >> ${outdir}/cmd_list_plots.cmds;
+done 1<${outdir}/datatables_files.txt 2<${outdir}/hbonds_files.txt 3<${outdir}/dist_files.txt
 
-#parallel --jobs ${cpus} < ${outdir}/cmd_list.cmds &> ${outdir}/run.macro_hbond.stdouterr
+parallel --jobs ${cpus} < ${outdir}/cmd_list_plots.cmds &> ${outdir}/run.macro_hbond_plots.stdouterr
 
-
-#parallel --jobs ${cpus} bash ${scriptsdir}/CollectMultipleMetricsForMultipleBams.sh {}  ::: ${dstDir}/star/*.Aligned.sortedByCoord.out.bam &
 
