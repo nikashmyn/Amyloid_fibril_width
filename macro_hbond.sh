67a086fdce179384d55aa05590796a737e810215
David Boyer <davidboyer@Davids-MacBook-Pro-4.local>
2021-06-30 17:00:36 -0700

    Re-ran program after renaming h_pdb directory

--- macro_hbond.sh
+++ macro_hbond.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 no_h_pdb_dir=/Users/davidboyer/Dropbox/Tau_Project_EISENBERG_LAB/amyloid_width/python/AB-Fibril-Radius-vs-Hydrogen-Bond-Relationship/pdb_noh/
-h_qscore_pdb_dir=/Users/davidboyer/Dropbox/Tau_Project_EISENBERG_LAB/amyloid_width/python/AB-Fibril-Radius-vs-Hydrogen-Bond-Relationship/pdb_h_qscore/
+h_pdb_dir=/Users/davidboyer/Dropbox/Tau_Project_EISENBERG_LAB/amyloid_width/python/AB-Fibril-Radius-vs-Hydrogen-Bond-Relationship/pdb_h/
 cpus=4
 scripts_dir=/Users/davidboyer/Dropbox/Tau_Project_EISENBERG_LAB/amyloid_width/python/AB-Fibril-Radius-vs-Hydrogen-Bond-Relationship/ #path to the folder which includes all auxiliary scripts
 output_graphs_dir=/Users/davidboyer/Dropbox/Tau_Project_EISENBERG_LAB/amyloid_width/python/AB-Fibril-Radius-vs-Hydrogen-Bond-Relationship/graphs/
@@ -31,23 +31,23 @@ parallel --jobs ${cpus} < ${no_h_pdb_dir}/cmd_list.cmds &> ${no_h_pdb_dir}/run.m
 echo "Done!"
 
 echo "Calculating beta-sheet hydrogen bond lengths and off-axis tilts for the following files:"
-echo `ls ${h_qscore_pdb_dir}/*.pdb` 
+echo `ls ${h_pdb_dir}/*.pdb` 
 
 #read in .pdb files to process
-cd ${h_qscore_pdb_dir}
+cd ${h_pdb_dir}
 ls *.pdb > pdb_files.txt
 
 #Reset the cmd list file
-echo "" > ${h_qscore_pdb_dir}/cmd_list.cmds
+echo "" > ${h_pdb_dir}/cmd_list.cmds
 
 #Generate Run script commands (cmds) for h-bonds csvs
 while read line  
 do
-  echo "python3 ${scripts_dir}/find_hbonds_04252021_debugged.py ${line} ${h_qscore_pdb_dir}" >> ${h_qscore_pdb_dir}/cmd_list.cmds;
-  echo "" >> ${h_qscore_pdb_dir}/cmd_list.cmds;
-done < ${h_qscore_pdb_dir}/pdb_files.txt
+  echo "python3 ${scripts_dir}/find_hbonds_04252021_debugged.py ${line} ${h_pdb_dir}" >> ${h_pdb_dir}/cmd_list.cmds;
+  echo "" >> ${h_pdb_dir}/cmd_list.cmds;
+done < ${h_pdb_dir}/pdb_files.txt
 
-parallel --jobs ${cpus} < ${h_qscore_pdb_dir}/cmd_list.cmds &> ${h_qscore_pdb_dir}/run.macro_hbond.stdouterr
+parallel --jobs ${cpus} < ${h_pdb_dir}/cmd_list.cmds &> ${h_pdb_dir}/run.macro_hbond.stdouterr
 
 echo "Done!"
 echo "I am now graphing your data :)"
@@ -56,8 +56,8 @@ echo "I am now graphing your data :)"
 echo "" > ${output_graphs_dir}/cmd_list_plots.cmds
 
 #read in successfully generated csvs 
-ls ${h_qscore_pdb_dir}/*datatable.csv > ${output_graphs_dir}/datatables_files.txt
-ls ${h_qscore_pdb_dir}/*hbonds.csv > ${output_graphs_dir}/hbonds_files.txt
+ls ${h_pdb_dir}/*datatable.csv > ${output_graphs_dir}/datatables_files.txt
+ls ${h_pdb_dir}/*hbonds.csv > ${output_graphs_dir}/hbonds_files.txt
 ls ${no_h_pdb_dir}/*dist.csv > ${output_graphs_dir}/dist_files.txt
 
 #Generate run script commands (cmds) for h-bond plots
